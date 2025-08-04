67785a8f1bd74b0a8251917eda25658612eb382f
Christopher Bryan <christopher_bryan@rocketmail.com>
2025-08-04 11:14:15 -0700

    modified users read access to allow unauthenticated reqs from admin UI and frontend with a shared secret only

--- src/collections/users/access/viewer.ts
+++ src/collections/users/access/viewer.ts
@@ -1,7 +1,7 @@
-import type { Access, Where } from 'payload'
+import type { Access } from 'payload'
 import { checkRole } from './check-role'
 
-export const viewer: Access = ({ req: { user, origin } }) => {
+export const viewer: Access = ({ req: { user, origin, payloadAPI, headers } }) => {
   if (user) {
     // checks if user is admin or editor
     if (checkRole(['admin', 'editor'], user)) {
@@ -16,5 +16,10 @@ export const viewer: Access = ({ req: { user, origin } }) => {
     }
   }
   // requests from the frontend can only see the first and last names of the user (using afterRead hook), else public users cannot see
-  return origin.includes(process.env.NEXT_PUBLIC_FRONTEND_URL!)
+  // this request can only be made only if headers contain a shared secret or it's being called from the admin UI, the payloadAPI property will still be 'REST'
+  // even if this access control is called internally because it was referenced as a relationship field in another collection
+  return (
+    payloadAPI === 'local' ||
+    headers.get('x-frontend-secret') === process.env.PAYLOAD_FRONTEND_SHARED_SECRET
+  )
 }
