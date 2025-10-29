4984a699103bc894c76680748a67a39a3e99bd4b
Christopher Bryan <christopher_bryan@rocketmail.com>
2025-10-28 21:52:54 -0700

    fix: added migrations for new partner locations options

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20250728_170203 from './20250728_170203';
 import * as migration_20250801_033003 from './20250801_033003';
 import * as migration_20250814_182659 from './20250814_182659';
 import * as migration_20251026_212237 from './20251026_212237';
+import * as migration_20251029_045228 from './20251029_045228';
 
 export const migrations = [
   {
@@ -22,6 +23,11 @@ export const migrations = [
   {
     up: migration_20251026_212237.up,
     down: migration_20251026_212237.down,
-    name: '20251026_212237'
+    name: '20251026_212237',
+  },
+  {
+    up: migration_20251029_045228.up,
+    down: migration_20251029_045228.down,
+    name: '20251029_045228'
   },
 ];
